46053a4a1b52d11a5ff5a6dc887e53795e12874b
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-04-22 17:39:29 -0300

    se logro que se diga quien es el mayor de los 3

--- js/11-CualEsElMayor.js
+++ js/11-CualEsElMayor.js
@@ -14,7 +14,6 @@ e indique el nombre del mayor. */
 // let nombreMayor = nombres[indiceMayor];
 // document.writeln(`El mayor es ${nombreMayor}`)
 
-
 let controladora = 1;
 let nombre1 = "";
 let edad1 = 0;
@@ -22,26 +21,32 @@ let nombre2 = "";
 let edad2 = 0;
 let nombre3 = "";
 let edad3 = 0;
-do{
+do {
   switch (controladora) {
     case 1:
-      nombre1 = prompt("Dime el nombre de la primera persona:")
-      edad1 = parseInt(prompt("Dime la edad de la primera persona:"))
+      nombre1 = prompt("Dime el nombre de la primera persona:");
+      edad1 = parseInt(prompt("Dime la edad de la primera persona:"));
       break;
     case 2:
-      nombre2 = prompt("Dime el nombre de la segunda persona:")
-      edad2 = parseInt(prompt("Dime la edad de la segunda persona:"))
+      nombre2 = prompt("Dime el nombre de la segunda persona:");
+      edad2 = parseInt(prompt("Dime la edad de la segunda persona:"));
       break;
     case 3:
-      nombre3 = prompt("Dime el nombre de la tercera persona:")
-      edad3 = parseInt(prompt("Dime la edad de la tercera persona:"))
+      nombre3 = prompt("Dime el nombre de la tercera persona:");
+      edad3 = parseInt(prompt("Dime la edad de la tercera persona:"));
       break;
-  
+
     default:
-      document.writeln("No se hallaron casos")
+      document.writeln("No se hallaron casos");
       break;
   }
   controladora++;
-}while (controladora <= 3)
+} while (controladora <= 3);
 
-document.writeln(nombre1)
+if (edad1 > edad2 && edad1 > edad3) {
+  alert(`El mayor de los estre es ${nombre1}`);
+} else if (edad2 > edad1 && edad2 > edad3) {
+  alert(`El mayor de los estre es ${nombre2}`);
+} else {
+  alert(`El mayor de los estre es ${nombre3}`);
+}
